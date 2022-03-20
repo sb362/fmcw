09590f84ea24cab8c3323c8289380508d88f327e
Stuart <sb362@st-andrews.ac.uk>
2022-03-20 23:53:35 +0000

    Add ring buffer

--- src/ringbuffer.h
+++ src/ringbuffer.h
@@ -5,10 +5,16 @@
 
 typedef struct ring_buffer_t ring_buffer_t;
 
-void ring_buffer_init(ring_buffer_t *buffer, size_t size);
-void ring_buffer_free(ring_buffer_t *buffer);
-void ring_buffer_clear(ring_buffer_t *buffer);
+ring_buffer_t *ring_buffer_init(size_t capacity, size_t buf_size);
+void ring_buffer_free(ring_buffer_t *rbuf);
+void ring_buffer_clear(ring_buffer_t *rbuf);
 
+void ring_buffer_write(ring_buffer_t *rbuf, double *buf, size_t bsize);
+double *ring_buffer_read(ring_buffer_t *rbuf);
+double *ring_buffer_peek(ring_buffer_t *rbuf);
+double *ring_buffer_peek_back(ring_buffer_t *rbuf, int n);
 
+size_t ring_buffer_size(ring_buffer_t *rbuf);
+size_t ring_buffer_capacity(ring_buffer_t *rbuf);
 
 #endif // RING_BUFFER_H
